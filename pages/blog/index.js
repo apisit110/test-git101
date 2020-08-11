dfae8d36f843e6d4d42c4e2a179ec5fad4eac6ca
apisit <apisitnowarat@gmail.com>
2020-08-11 16:35:58 +0700

    update Blog Page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,12 @@
+import Layout from "../../components/Layout"
+
 const Blog = function ({ ...props }) {
     return (
         <>
-            <h1>Blog</h1>
-            <p>{props.descriptions}</p>
+            <Layout title="Blog" desc="Hi This is Blog Page">
+                <h1>Blog Page</h1>
+                <p>{props.descriptions}</p>
+            </Layout>
         </>
     )
 }
